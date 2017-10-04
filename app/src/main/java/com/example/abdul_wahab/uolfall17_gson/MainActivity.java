e67800b97eaf6f40caadc060e175aea2272463a9
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-10-04 10:42:14 +0500

    gson serialize deserialize

--- app/src/main/java/com/example/abdul_wahab/uolfall17_gson/MainActivity.java
+++ app/src/main/java/com/example/abdul_wahab/uolfall17_gson/MainActivity.java
@@ -8,22 +8,29 @@ import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MTAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Contact c = new Contact(1, "abc", true);
-
-
         Gson gson = new Gson();
-
-        gson.toJson(c);
-
+        /*Contact c = new Contact(1, "abc", true);
 
 
+        String str = gson.toJson(c);
+        Log.d(TAG, "Contact JSON is  " + str);
+        */
+
+        String str = "{\"gender\":true,\"id\":1,\"name\":\"abc\"}";
+
+        Contact c = gson.fromJson(str, Contact.class);
+
+        String s = "";
 
 
+// Intent intent ;
 
 
     }
